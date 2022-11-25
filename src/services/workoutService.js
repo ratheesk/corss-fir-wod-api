b534d408dbd146923efb9fb6813386ee3278fab4
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-25 23:26:39 +0530

    Upfate methods

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -2,38 +2,39 @@ const { v4: uuid } = require("uuid");
 const Workout = require("../database/Workout");
 
 const getAllWorkouts = () => {
-    const allWorkouts = Workout.getAllWorkouts();
-    return allWorkouts;
-  };
-  
-  const getOneWorkout = () => {
-    return;
-  };
-  
-  const createNewWorkout = (newWorkout) => {
-    const workoutToInsert = {
-        ...newWorkout,
-        id: uuid(),
-        createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
-        updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
-      };
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
 
-    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
-    return createdWorkout;
+const getOneWorkout = (workoutId) => {
+  const workout = Workout.getOneWorkout(workoutId);
+  return workout;
+};
+
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
   };
-  
-  const updateOneWorkout = () => {
-    return;
-  };
-  
-  const deleteOneWorkout = () => {
-    return;
-  };
-  
-  module.exports = {
-    getAllWorkouts,
-    getOneWorkout,
-    createNewWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
-  };
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+  return createdWorkout;
+};
+
+const updateOneWorkout = (workoutId, changes) => {
+  const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
+  return updatedWorkout;
+};
+
+const deleteOneWorkout = (workoutId) => {
+  Workout.deleteOneWorkout(workoutId);
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
