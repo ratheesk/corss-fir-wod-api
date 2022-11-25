63749bf7b8f3c0457b6d0058d207b4fb7dbc8517
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-26 00:12:46 +0530

    add filters

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,12 +1,19 @@
 const DB = require("./db.json");
 const { saveToDatabase } = require("./utils");
 
-const getAllWorkouts = () => {
-  try {
-    return DB.workouts;
-  } catch (error) {
-    throw { status: 500, message: error };
-  }
+const getAllWorkouts = (filterParams) => {
+    try {
+        let workouts = DB.workouts;
+        if (filterParams.mode) {
+          return DB.workouts.filter((workout) =>
+            workout.mode.toLowerCase().includes(filterParams.mode)
+          );
+        }
+        // Other if-statements will go here for different parameters
+        return workouts;
+      } catch (error) {
+        throw { status: 500, message: error };
+      }
 };
 
 const getOneWorkout = (workoutId) => {
