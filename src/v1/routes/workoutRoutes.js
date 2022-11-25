5a1ef64d74ad55f76f83f906abda92554b8676a4
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-26 00:15:35 +0530

    install apicache

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
+const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
+
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/", workoutController.getAllWorkouts);
 
