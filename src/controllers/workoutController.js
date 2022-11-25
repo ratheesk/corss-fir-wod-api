bfb725296708447705b6d6d56396dd90eda2e39b
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-25 23:34:00 +0530

    add error handeling

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -43,8 +43,14 @@ const createNewWorkout = (req, res) => {
     exercises: body.exercises,
     trainerTips: body.trainerTips,
   };
-  const createdWorkout = workoutService.createNewWorkout(newWorkout);
-  res.status(201).send({ status: "OK", data: createdWorkout });
+  try {
+    const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.status(201).send({ status: "OK", data: createdWorkout });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const updateOneWorkout = (req, res) => {
