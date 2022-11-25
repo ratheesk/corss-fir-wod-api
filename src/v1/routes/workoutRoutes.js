59f5613841ca8c6505fdb28f0cc00ec45629ae30
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-11-26 00:31:10 +0530

    add errors in the document

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -7,12 +7,19 @@ const recordController = require("../../controllers/recordController");
 const router = express.Router();
 const cache = apicache.middleware;
 
+
 /**
  * @openapi
  * /api/v1/workouts:
  *   get:
  *     tags:
  *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
  *     responses:
  *       200:
  *         description: OK
@@ -27,7 +34,23 @@ const cache = apicache.middleware;
  *                 data:
  *                   type: array 
  *                   items: 
- *                     type: object
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
  */
 
 router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
